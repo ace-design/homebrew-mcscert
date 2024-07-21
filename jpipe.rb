0f8def58706539a13dc2aa0aa046e85b153f046e
Sébastien Mosser <sebastien.mosser@gmail.com>
2024-07-20 20:03:45 -0400

    fix script and compiler contents (yes, almost everything...)

--- jpipe.rb
+++ jpipe.rb
@@ -5,10 +5,10 @@ class Jpipe < Formula
     homepage "https://github.com/ace-design/jpipe"
 
     url "https://github.com/ace-design/jpipe/releases/download/0.2.0/jpipe-0.2.0.tar.gz"
-    sha256 "8dfec2de4a224d00981824384f469c6ca0fb332511d51f14e4b6b7a6fdc859c8"
+    sha256 "8cb6e431e63d0cf43f8e6d6f9cece4facecec0dcd71b293819e7fa1ec36a6a77"
 
     depends_on "graphviz"
-    depends_on "openjdk"
+    depends_on "openjdk@21"
 
 
     def install
